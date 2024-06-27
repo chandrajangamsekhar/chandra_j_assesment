29f1437330636a3507247ff6b8a27874b04fa8ac
cjangam <chandra.jangam@lendstreet.com>
2024-06-27 16:38:02 +0530

    Negative numbers exception changes

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
+# lib/calculator.rb
 class Calculator
   def add(str)
     return 0 if str.empty?
 
     delimiters = /,|\n/
     numbers = str.split(delimiters).map(&:to_i)
+
+    negative_numbers = numbers.select(&:negative?)
+
+    raise "negative numbers are not allowed: #{negative_numbers.join(', ')}" unless negative_numbers.empty?
+
     numbers.sum
   end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # spec/calculator_spec.rb
 require 'rspec'
 require_relative '../lib/calculator'
@@ -7,13 +9,25 @@ RSpec.describe Calculator do
     context 'when the input is a comma-separated string of numbers' do
       it 'returns the sum of the numbers' do
         calculator = Calculator.new
-        expect(calculator.add("1,2,4")).to eq(7)
+        expect(calculator.add('1,2,4')).to eq(7)
       end
     end
+
     context 'when the input is a single string of numbers' do
       it 'returns the sum of the numbers' do
         calculator = Calculator.new
-        expect(calculator.add("4")).to eq(4)
+        expect(calculator.add('4')).to eq(4)
+      end
+    end
+
+    context 'when the input contains negative numbers' do
+      it 'raises an error listing all negative numbers' do
+        calculator = Calculator.new
+        begin
+          calculator.add('1,-2,3')
+        rescue RuntimeError => e
+          expect(e.message).to eq('negative numbers are not allowed: -2')
+        end
       end
     end
   end
